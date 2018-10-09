85170725ecaccc0b78fc0261e69f03f28c35d90b
hansol13 <michaelbaek96@gmail.com>
2018-10-09 14:07:43 -0400

    adjust server.js and package.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
 
 app.get('/api/catechism_answers', (req, res) => {
   const catechism_answers = [
@@ -331,16 +331,19 @@ app.get('/api/catechism_answers', (req, res) => {
   res.json(catechism_answers);
 });
 
-const port = 5000;
+// use routes
+app.use('/api/catechism_answers', catechism_answers);
 
-
-if(process.env.NODE_ENV === 'production'){
-  //set static folder
+// serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-//app.listen(port, () => `Server running on port ${port}`);
-app.listen(process.env.PORT || 5000);
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => `Server running on port ${port}`);
